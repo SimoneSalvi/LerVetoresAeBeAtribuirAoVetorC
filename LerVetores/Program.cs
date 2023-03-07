2271f92cc6c894e8eb670cb49c76173e83d4be51
SimoneSalvi <simonebonemer@yahoo.com.br>
2023-03-07 09:53:05 -0300

    Add project files.

---
+++ LerVetores/Program.cs
@@ -0,0 +1,58 @@
+﻿NewMethod();
+
+static void NewMethod()
+{
+    int[] A = new int[5];
+    int[] B = new int[5];
+    int[] C = new int[10];
+    int cont = 0;
+
+//vetor a
+    for ( cont = 0; cont < A.Length; cont++)
+    {
+        Console.WriteLine("Informe o numero:");
+        A[cont] = int.Parse(Console.ReadLine());
+    }
+
+    Console.WriteLine("Vetor a");
+    for (cont = 0; cont < A.Length; cont++)
+    {
+        Console.WriteLine(A[cont]);
+       
+    }
+
+//vetor b
+    for (cont = 0; cont < B.Length; cont++)
+    {
+        Console.WriteLine("Informe o numero:");
+        B[cont] = int.Parse(Console.ReadLine());
+    }
+
+    Console.WriteLine("Vetor b");
+    for (cont = 0; cont < B.Length; cont++)
+    {
+        Console.WriteLine(B[cont]);
+
+    }
+
+//vetor c
+
+    for (cont = 0 ; cont < C.Length-5; cont++)
+    {
+        C[cont] = A[cont];
+    }
+    for (cont = 5 ; cont < C.Length; cont++)
+    {
+        C[cont] = B[cont-5];
+    }   
+
+
+
+    Console.WriteLine("Vetor c");
+    for (cont = 0; cont < C.Length; cont++)
+    {
+        Console.WriteLine(C[cont]);
+
+    }
+
+}
